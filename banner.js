e35c44a59b51beeab6704881d38e35b312c49925
tkwkrhcn <tkwkrhcn11@gmail.com>
2025-05-07 13:34:57 +0800

    banner.js

--- banner.js
+++ banner.js
@@ -1,5 +1,5 @@
-<!-- ✅ 1. HTML에 미리 렌더링할 배너 2개 (eager로 LCP 최적화) -->
 <div id="banner-container">
+  <!-- LCP 최적화: 미리 출력되는 2개 배너 -->
   <div class="banner">
     <a href="https://a8l-audi.com/" target="_blank" rel="nofollow noopener noreferrer">
       <img src="https://imagedelivery.net/YBuUVvHrWBzVF83Na77hDQ/42adcbce-2231-4973-8e14-0ee48c8b3f00/public" alt="a8l-audi.com" loading="eager">
@@ -12,9 +12,9 @@
   </div>
 </div>
 
-<!-- ✅ 2. 나머지 배너는 JS로 lazy 로딩 -->
 <script>
 (function () {
+  // 스타일 삽입
   const style = document.createElement('style');
   style.textContent = `
     #banner-container {
@@ -52,37 +52,32 @@
   `;
   document.head.appendChild(style);
 
-  const fixedBanners = [
-    { url: "https://xn--p49al7tolbl8o8tj.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/ff2d6728-7523-4452-bd48-ecdb8072a600/public" },
-    { url: "https://xn--vy7ba476b.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/c4352a78-3a5f-42ce-5255-5f9be9ced200/public" },
-    { url: "https://xn--he5b11d80l.kr/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/182b8e5f-e5b7-494b-dd67-d567235fad00/public" },
-    { url: "https://xn--he5b11d80l.kr/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/182b8e5f-e5b7-494b-dd67-d567235fad00/public" },
-    { url: "https://xn--k01bo01c.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/e7ffbf88-05fc-43cb-c033-2b0419995d00/public" }
-  ];
-
-  const banners = [
+  const additionalBanners = [
     { url: "https://1bet1.one/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/4f4a6af4-213f-4dd0-38e4-db20582f8c00/public" },
     { url: "https://winner1.site/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/c517cdb9-359c-417e-2506-9e3c34382600/public" },
     { url: "https://lula.asia/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/b3379438-2c35-4ba0-e4ef-a6ca90e6b100/public" },
     { url: "https://onetop.space/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/ca17535c-ddac-4664-4dcd-da171cec5d00/public" },
     { url: "https://xn--jp2bl9m0na51v.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/3e2b4101-5992-47fa-3ed5-31aeb4f0df00/public" },
     { url: "http://rc337.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/20199e87-2b76-4011-2412-fcb5c5164a00/public" },
-    { url: "http://dg745.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/ecf0c844-3ea7-48e1-d565-ba1cadf15900/public" }
+    { url: "http://dg745.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/ecf0c844-3ea7-48e1-d565-ba1cadf15900/public" },
+    { url: "https://xn--p49al7tolbl8o8tj.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/ff2d6728-7523-4452-bd48-ecdb8072a600/public" },
+    { url: "https://xn--vy7ba476b.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/c4352a78-3a5f-42ce-5255-5f9be9ced200/public" },
+    { url: "https://xn--he5b11d80l.kr/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/182b8e5f-e5b7-494b-dd67-d567235fad00/public" },
+    { url: "https://xn--k01bo01c.com/", img: "https://imagedelivery.net/hn8cyNBhDj7fHt_rfVXsFQ/e7ffbf88-05fc-43cb-c033-2b0419995d00/public" }
   ];
 
+  // 셔플 함수
   function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
   }
-  shuffle(banners);
 
-  const finalBanners = [...banners, ...fixedBanners];
-
+  shuffle(additionalBanners);
   const bannerContainer = document.getElementById('banner-container');
   if (bannerContainer) {
-    finalBanners.forEach((banner, index) => {
+    additionalBanners.forEach((banner) => {
       const a = document.createElement('a');
       a.href = banner.url;
       a.target = "_blank";
@@ -92,8 +87,7 @@
       img.src = banner.img;
       img.loading = "lazy";
       try {
-        const domain = new URL(banner.url).hostname;
-        img.alt = domain;
+        img.alt = new URL(banner.url).hostname;
       } catch {
         img.alt = "무료웹툰";
       }
